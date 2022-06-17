9b13cba632baa8bb5538d3f94b0a73efe15200a7
Pulkit Samrat <105787720+pulkitSamrat@users.noreply.github.com>
2022-06-17 17:52:36 +0530

    Update pulkit.java

--- pulkit.java
+++ pulkit.java
@@ -1,6 +1,7 @@
 class pulkit{  
     public static void main(String args[]){  
      System.out.println("Hello Java 1");  
-     System.out.println("Hello Java 2"); 
+     System.out.println("Hello Java 2");
+     System.out.println("Hello Java 3");
     }  
 }  
